0de4e96a7d99cc637640b46d5972fd8fe3413b6a
Jeff Millar <jeff.millar@sbcglobal.net>
2015-02-22 22:25:21 -0800

    First commit

---
+++ strings/solution_ 1.rb
@@ -0,0 +1,1 @@
+puts "Jeff" + "Millar"
---
+++ strings/solution_2.rb
@@ -0,0 +1,10 @@
+whole_numeber = 7853
+thousands = whole_number / 1000
+hundreds = whole_number % 1000 / 100
+tens = whole_number % 1000 % 100 / 10
+ones = whole_number % 1000 %100 % 10
+puts "The whole number is #{whole_number}"
+puts "The thousands is #{thousands}"
+puts "The hundreds is #{hundreds}"
+puts "The tens is #{tens}"
+puts "The ones is #{ones}"

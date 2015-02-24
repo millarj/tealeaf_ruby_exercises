3f18183aee7b7b8d669e7b0a0bb3dca9e8446cf3
Jeff Millar <jeff.millar@sbcglobal.net>
2015-02-23 20:38:07 -0800

    Completed variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Ener your last name:"
+last_name = gets.chomp
+puts "#{first_name} #{last_name} is a great name!"
+
+10.times do
+  puts "#{first_name} #{last_name}"
+end

ab306376090d89fce91df552c6aea0f4dd07404e
Jeff Millar <jeff.millar@sbcglobal.net>
2015-02-23 19:28:12 -0800

    Completed string exercises

--- strings/solution_3.rb
+++ strings/solution_3.rb
@@ -1,1 +1,8 @@
-puts "Jeff" + " Millar"
+coolest_guy_movies = {"Dumb and Dumber" => 2012, "21 Jumpstreet" => 2010, "The Interview" => 2014, "Dude, Where's My Car?" => 2005, "The Hangover" => 2010}
+puts coolest_guy_movies["Dumb and Dumber"]
+puts coolest_guy_movies["21 Jumpstreet"]
+puts coolest_guy_movies["The Interview"]
+puts coolest_guy_movies["Dude, Where's My Car?"]
+puts coolest_guy_movies["The Hangover"]
+puts coolest_guy_movies.class
+
---
+++ strings/solution_4.rb
@@ -0,0 +1,3 @@
+coolest_guy_movies = [2012, 2010, 2014, 2005, 2010]
+puts coolest_guy_movies[0..4]
+puts coolest_guy_movies.class
